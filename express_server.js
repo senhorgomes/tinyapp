0b5d5fe03b16b4ae4c5a80a14c8023d442d5dbca
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-08 22:27:49 +0000

    Added a json to change our database object into a json string

--- express_server.js
+++ express_server.js
@@ -15,3 +15,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
