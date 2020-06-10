d2bcf8a5adf53899668174e80882637957293226
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-10 18:26:50 +0000

    Organized a bit

--- express_server.js
+++ express_server.js
@@ -11,14 +11,21 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// If we have a GET request asking for the path of '/', do the callback
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+
+//---------------LISTEN APPS------------------------------
+
 // Trigger a listen action, on a specific port (8080) and do a callback if it worked
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+//---------------GET APPS------------------------------
+
+// If we have a GET request asking for the path of '/', do the callback
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 // If we have a GET request, asking for /urls.json, we do the callback
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -27,46 +34,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-// An action script that creates a new shortURL
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  let longURL = req.body.longURL
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
 
 //Indexof all short URLs
 app.get("/urls", (req, res) => {
   let templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-//Creation of new URL. Not the actual act of it, just the page that hosts it.
-app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
-  res.render("urls_new", templateVars);
-});
-//Delete a page whenever called upopn.
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
-});
-//Allows you to log into the server
-app.post("/login", (req,res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
-});
-//Allows user to logout from server by clearing cookies
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
-})
 
-//Whenever called on, it will take in the information of the existing longURL, and allows you to edit it
-app.post("/urls/:shortURL", (req, res) => {
-  let longURL = req.body.longURL
-  urlDatabase[req.params.shortURL] = longURL;
-  res.redirect("/urls");
-})
 // In the event of a GET request, asking for /urls/somethingIDontKnowYet, do the callback
 // :shortURL is a route parameter, accessible in req.params (like a wildcard)
 app.get("/urls/:shortURL", (req, res) => {
@@ -84,6 +58,47 @@ app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL, templateVars);
 });
+
+//---------------POST APPS------------------------------
+
+// An action script that creates a new shortURL
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+//Creation of new URL. Not the actual act of it, just the page that hosts it.
+app.get("/urls/new", (req, res) => {
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
+});
+
+//Allows you to log into the server
+app.post("/login", (req,res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+//Allows user to logout from server by clearing cookies
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+//Delete a page whenever called upopn.
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+//Whenever called on, it will take in the information of the existing longURL, and allows you to edit it
+app.post("/urls/:shortURL", (req, res) => {
+  let longURL = req.body.longURL
+  urlDatabase[req.params.shortURL] = longURL;
+  res.redirect("/urls");
+})
+
+//-----------Functions (to be exported and imported in future update)------------
+
 //Chracter set uses the same format as the diceRoller to find a random number. Except this will go through the character length ot characterSet, return one number, and add it to the string. This will happen 6 times, and it will return a full string.
 function generateRandomString() {
   let url = "";
